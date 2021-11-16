43e58d545050655085786a9332045ac1d7b39480
Yash Chaudhari <yash_chaudhari@live.com>
2021-11-16 15:24:59 +0530

    Added more illustrations in homepage and added Banner Component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import illus from '../../assets/illus.jpg'
+import illus1 from '../../assets/illus1.jpg'
+import illus2 from '../../assets/illus2.jpg'
+import illus3 from '../../assets/illus3.jpg'
+import illus4 from '../../assets/illus4.jpg'
+import Banner from './Banner'
 function Home() {
     return (
-        <div style={{ backgroundColor: "#e0e0e0" }}>
-            <header >
+        <div >
+            <header style={{ backgroundColor: "#e0e0e0" }}>
                 <div className="container px-5">
                     <div className="row gx-5 align-items-center">
                         <div className="col-lg-6">
@@ -19,64 +23,18 @@ function Home() {
                         </div>
                         <div className="col-lg-6">
                             <div>
-                                <img src={illus} className="img-fluid" height="500" width="500"></img>
+                                <img src={illus1} className="img-fluid" height="500" width="500"></img>
                             </div>
                         </div>
                     </div>
                 </div>
             </header>
-            <hr style={{ margin: '0px' }} />
-            <div className="container px-5" >
-                <div className="row gx-6 align-items-center" style={{ height: "400px" }}>
-                    <div className="col-lg-6">
-                        <div>
-                            <i class="bi bi-graph-up-arrow" style={{ fontSize: 150 }}></i>
-                        </div>
-                    </div>
-                    <div className="col-lg-6">
-                        <div className="mb-5 mb-lg-0 text-center text-lg-start">
-                            <h1 className="display-2 lh-1 mb-3">Track your stats as you improve.</h1>
-                            <p className="lead fw-normal text-muted mb-5">Start Learning with EduManage Today.</p>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-            <hr style={{ margin: '0px' }} />
-            <div className="container px-5" >
-                <div className="row gx-6 align-items-center" style={{ height: "400px" }}>
-                    <div className="col-lg-6">
-                        <div className="mb-5 mb-lg-0 text-center text-lg-start">
-                            <h1 className="display-2 lh-1 mb-3">Earn badges to gain confidence.</h1>
-                            <p className="lead fw-normal text-muted mb-5">Start Learning with EduManage Today.</p>
-                        </div>
-                    </div>
-                    <div className="col-lg-6">
-                        <div>
-                            <i class="bi bi-patch-check" style={{ fontSize: 150 }}></i>
-                        </div>
-                    </div>
-
-
-                </div>
-            </div>
-            <hr style={{ margin: '0px' }} />
-            <div className="container px-5" >
-                <div className="row gx-6 align-items-center" style={{ height: "400px" }}>
-                    <div className="col-lg-6">
-                        <div>
-                            <i class="bi bi-lightning-charge" style={{ fontSize: 150 }}></i>
-                        </div>
-                    </div>
-                    <div className="col-lg-6">
-                        <div className="mb-5 mb-lg-0 text-center text-lg-start">
-                            <h1 className="display-2 lh-1 mb-3">Concise and easy to use.</h1>
-                            <p className="lead fw-normal text-muted mb-5">Start Learning with EduManage Today.</p>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
+            <hr style={{ margin: 0 }} />
+            <Banner pos={1} illusImage={illus2} titleLine={"Track your stats as you improve."}  subtitleLine={"Start Learning with EduManage Today."}/>
+            <hr style={{ margin: 0 }} />
+            <Banner pos={0} illusImage={illus4} titleLine={"Earn badges to gain confidence."}  subtitleLine={"Start Learning with EduManage Today."}/>
+            <hr style={{ margin: 0 }} />
+            <Banner pos={1} illusImage={illus3} titleLine={"Concise and easy to use."}  subtitleLine={"Start Learning with EduManage Today."}/>
         </div>
     )
 }
